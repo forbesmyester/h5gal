cc55d7e67e291a616e92a42282ab4520fffd1c6e
Matthew Forrester <matt@keyboardwritescode.com>
2013-10-28 09:34:25 +0000

    Progress and some docs

--- src/h5gal/core.clj
+++ src/h5gal/core.clj
@@ -1,6 +1,42 @@
 (ns h5gal.core
   (:gen-class))
 
+;;;; Gallery Generator built to learn Clojure
+
+;;; Directory strucutre example:
+;;;
+;;;     <datadir>/2011/04/28/Tokyo Trip/Picture3425.jpg
+;;;
+;;; The location of photo's, the first three levels of directory outside of the
+;;; <datadir> are teh date in reverse order. The next "Tokyo Trip" is the name
+;;; of the specific event. These combined should give enough information to
+;;; supply a title and a date of a set of pictures, which might be enough for
+;;; lots of people.
+;;;
+;;;     <datadir>/2011/04/28/Tokyo Trip/README.md
+;;;
+;;; Should you wish to describe the event in detail you can include a README
+;;; within the event directory. It should be formatted using Markdown.
+;;;
+;;;     <datadir>/2011/04/28/Tokyo Trip/Picture3425/README.md
+;;;
+;;; If you want to add a description of what is in the individual picture
+;;; create a directory of the same name, but without the extension and add a
+;;; README file there.
+;;;
+;;;     <datadir>/2011/04/28/Tokyo Trip/Picture3425/_default.jpg
+;;;     <datadir>/2011/04/28/Tokyo Trip/Picture3425/_thumb_small.jpg
+;;;     <datadir>/2011/04/28/Tokyo Trip/Picture3425/_thumb_medium.jpg
+;;;     <datadir>/2011/04/28/Tokyo Trip/Picture3425/_thumb_large.jpg
+;;;
+;;; NOTE: These files will be created automatically for displaying within the
+;;; browser.
+;;;
+;;;     <datadir>/2011/04/28/Tokyo Trip/Picture3425/override.jpg
+;;;
+;;; But if it seems a file called override it will show this within the browser
+;;; instead as well as use it for thumbnails.
+
 (use '[clojure.java.io])
 (use 'clj-time.core)
 (import 'java.util.Locale)
